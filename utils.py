d77b767e325043e4734d67990cf6ab255d799cd5
arpit <arpitshukla62@gmail.com>
2022-05-16 22:33:03 +0530

    Customize personal message

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def get_personal_message() -> str:
     # Modify the personal message as desired.
-    name: str = "___"
-    return f"Hello, my name's {name}."
+    name: str = "Arpit Shukla"
+    return f"Hello, my name is {name}."

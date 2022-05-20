38e8a7df5aa2d9cc9caf1594267dd4aae447671b
samyak-ti <samyak.samdariya@trilogy.com>
2022-05-20 19:18:32 +0530

    Added Personal Message

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def get_personal_message() -> str:
     # Modify the personal message as desired.
-    name: str = "___"
-    return f"Hello, my name's {name}."
+    name: str = "Samyak"
+    return f"Hello, my name's {name} and this is my first assignment"

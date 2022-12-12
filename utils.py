1b641798206abdc8d9f07bc8c9498dbe7ac00f0f
chirag-ti <chirag.mittal@trilogy.com>
2022-12-12 16:57:48 +0000

    personal msg

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def get_personal_message() -> str:
     # Modify the personal message as desired.
-    name: str = "___"
-    return f"Hello, my name's {name}."
+    name: str = "chirag"
+    return f"Hello, my name's {name}. Excited to work with you all"
